7fd05c81510b63254e04f2ad10fe8c3403a58e5a
Sam Beevors <sambeevors@googlemail.com>
2024-11-11 00:47:56 +0000

    fix: suspense boundary

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,5 +1,19 @@
 import Auth from '@/components/Auth'
 
+import { Loader } from 'lucide-react'
+import { Suspense } from 'react'
+
 export default function AuthPage() {
-  return <Auth />
+  return (
+    <Suspense
+      fallback={
+        <div className="flex items-center justify-center gap-2">
+          <Loader className="w-4 h-4 animate-spin" />
+          Loading...
+        </div>
+      }
+    >
+      <Auth />
+    </Suspense>
+  )
 }
